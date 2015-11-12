a85aa38a99070339410432b8b139e511daa30245
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-11-12 23:25:54 +0000

    looks for the available wifi adapters. take a random free one and create the ad-hoc network file

--- bin/setUpWireless.sh
+++ bin/setUpWireless.sh
@@ -1,7 +1,39 @@
-FILE=myDescription
+listFileWiFi=listWiFiInterfaces
+descriptionfile=myDescription
 i=0;
 
-cat $FILE | while read line; do
-  echo "$line"
+while read line; do
+  name="$line";
   i=i+1
-done
+done < $descriptionfile
+
+OIFS=$IFS;
+IFS=" ";
+
+#take the lines in the respective file
+while read line; do
+  listWiFi=($line);
+  echo "Taking random WiFi adapter available"
+  #find the number of elements in the respetive line
+  numberWiFiInterface=${#listWiFi[*]}
+  WiFi=${listWiFi[$((RANDOM%numberWiFiInterface))]}
+  echo "$WiFi will be used"
+done < $listFileWiFi
+IFS=$OIFS;
+
+echo "[connection]
+id=$name
+uuid=`uuidgen`
+type=802-11-wireless
+autoconnect=false
+
+[802-11-wireless]
+ssid=$name
+mode=ap
+mac-address=`ifconfig $WiFi | awk '/HWaddr/ {print $5}'`
+
+[ipv6]
+method=auto
+
+[ipv4]
+method=shared" > $name
