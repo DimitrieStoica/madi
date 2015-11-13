3a41d8175dec657d171cbe400cb9ca56b9ef9d70
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-11-13 14:53:44 +0000

    tries to connect the specified WiFi until connection established

--- bin/connectToWifi.sh
+++ bin/connectToWifi.sh
@@ -5,13 +5,27 @@ echo "Searching for available WiFi networks"
 #search for all available WiFi networks
 
 WiFiAvailableNetworks=(`sudo iw wlan1 scan | grep SSID | awk {'print $2'}`)
-value="QandA"
 
-for element in "${WiFiAvailableNetworks[@]}"
-do
-  if [ "$element" = "$value" ]; then
-    echo "Connecting shortly to $value" 
-  else
-    echo "I was not able to find WiFi pair"
-  fi
+descriptionfile=myDescription
+i=0;
+
+while read line; do
+  name="$line";
+  i=i+1
+done < $descriptionfile
+
+until `nmcli d wifi connect $name`; do
+  echo "Connecting to $name"
+  echo -ne '#####                     (20%)\r'
+  sleep 2
+  echo -ne '##########                (40%)\r'
+  sleep 2
+  echo -ne '###############           (60%)\r'
+  sleep 2
+  echo -ne '####################      (80%)\r'
+  sleep 2
+  echo -ne '######################### (100%)\r'
+  echo -ne '\n'
+  echo "Connection was established"
+  sleep 20
 done
