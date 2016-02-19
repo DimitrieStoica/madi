0540dcc46709865f4e4004bf8181cf2e47d1a1b5
Alex Stoica <dimitrie.stoica@gmail.com>
2016-02-19 20:37:11 +0000

    client knows buffer size that it will receive

--- src/controller/sockets/client/client.cpp
+++ src/controller/sockets/client/client.cpp
@@ -9,52 +9,41 @@ using namespace std;
 class Client {
   public:
 
-int getData(string ip, string port) {
-  try
-  {
-    // the user should specify the server - the 2nd argument
-    boost::asio::io_service io_service;
-
-    // Convert the server name that was specified as a parameter to the application, to a TCP endpoint.
-    tcp::resolver resolver(io_service);
-
-    tcp::resolver::query query(ip, port);
-
-    // The list of endpoints is returned using an iterator of type ip::tcp::resolver::iterator.
-    // A default constructed ip::tcp::resolver::iterator object can be used as an end iterator.
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-    // Now we create and connect the socket.
-    tcp::socket socket(io_service);
-    boost::asio::connect(socket, endpoint_iterator);
-
-    // The connection is open. All we need to do now is read the response from the daytime service.
-    for (;;)
+  char* getData(string ip, string port) {
+    try
     {
+      // the user should specify the server - the 2nd argument
+      boost::asio::io_service io_service;
+
+      // Convert the server name that was specified as a parameter to the application, to a TCP endpoint.
+      tcp::resolver resolver(io_service);
+
+      tcp::resolver::query query(ip, port);
+
+      // The list of endpoints is returned using an iterator of type ip::tcp::resolver::iterator.
+      // A default constructed ip::tcp::resolver::iterator object can be used as an end iterator.
+      tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+      // Now we create and connect the socket.
+      tcp::socket socket(io_service);
+      boost::asio::connect(socket, endpoint_iterator);
+
+      // The connection is open.
       // We use a boost::array to hold the received data.
-      boost::array<char, 128> buf;
       boost::system::error_code error;
 
       // The boost::asio::buffer() function automatically determines
       // the size of the array to help prevent buffer overruns.
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+      std::vector<char> buf(socket.available());
+      boost::asio::read(socket, boost::asio::buffer(buf));
 
-      cout  << buf.data() << endl;
-
-      //if connection closed by server return error
-      if (error == boost::asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw boost::system::system_error(error); // Some other error.
+      return buf.data();
     }
-  }
-  // handle any exceptions that may have been thrown.
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
-
-  return 0;
+    // handle any exceptions that may have been thrown.
+    catch (std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+    }
+    return 0;
+  };
 };
-};
-
