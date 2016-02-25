5b72d91388af4a34b9238ccd1bcd8efff2f177b7
Alex Stoica <stoica.dimitrie@gmail.com>
2016-02-25 13:45:01 +0000

    it only start or connect to WAN

--- src/bin/networkManagement.cpp
+++ src/bin/networkManagement.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include "Helpers/createWANfile.cpp"
-#include "Helpers/identifyNetworkHardware.cpp"
+#include "helpers/createWANfile.cpp"
+#include "helpers/identifyNetworkHardware.cpp"
 
-using namespace std;
-
-class networkManagement{
+class networkManagement {
 
   public: 
 
@@ -20,26 +18,25 @@ class networkManagement{
     string str;
 
     for (int i=0; i < resultVector.size(); i++) {
-    cout << "..........." << endl;
-    cout << resultVector[i] << endl;
-    str = co -> getOutputFromConsole("iw " + resultVector[i] + " link");
-    if (str.find("Not connected.") != string::npos) {
-      cout << resultVector[i] + " is free and can be used for setting up a WAN connection" << endl;
 
-      cwf -> writeToFile(resultVector[i], "robotino");
+      cout << resultVector[i] << endl;
+      str = co -> getOutputFromConsole("iw " + resultVector[i] + " link");
+      if (str.find("Not connected.") != string::npos) {
+        cout << resultVector[i] + " is free and can be used for setting up a WAN connection" << endl;
 
-      co -> getOutputFromConsole("chmod 600 " + myName);
-      co -> getOutputFromConsole("sudo cp " + myName + " /etc/NetworkManager/system-connections/");
-      co -> getOutputFromConsole("sudo rm " + myName);
-      co -> getOutputFromConsole("sudo /etc/init.d/networking restart");
-      co -> getOutputFromConsole("nmcli con up id " + myName + " iface " + resultVector[i]);
+        cwf -> writeToFile(resultVector[i], "robotino");
+
+        co -> getOutputFromConsole("chmod 600 " + myName);
+        co -> getOutputFromConsole("sudo cp " + myName + " /etc/NetworkManager/system-connections/");
+        co -> getOutputFromConsole("sudo rm " + myName);
+        co -> getOutputFromConsole("sudo /etc/init.d/networking restart");
+        co -> getOutputFromConsole("nmcli con up id " + myName + " iface " + resultVector[i]);
       
-    } else {
-      cout << "Hardware is busy for a WAN connection" << endl;
-      return 1;
+      } else {
+        cout << "Hardware is busy for a WAN connection" << endl;
+        return 1;
+      };
     };
-  };
-
     return 0;
   };
 
@@ -53,7 +50,6 @@ class networkManagement{
     string str;
 
     for (int i=0; i < resultVector.size(); i++) { 
-      cout << "..........." << endl;
       cout << resultVector[i] << endl;
       str = co -> getOutputFromConsole("iw " + resultVector[i] + " link");
       if (str.find("Not connected.") != string::npos) {
@@ -61,24 +57,6 @@ class networkManagement{
         co -> getOutputFromConsole("nmcli d wifi connect " + myName);
       } else {
         cout << "Hardware is busy for a WAN connection" << endl;
-      };
-    };
-    return 0;
-  };
-
-  int scanNetworkWAN(string myName) {
-    consoleOutput *co  = new consoleOutput();
-
-    vector <string> resultVector = searchForNetworkHardware();
-    string str;
-
-    for (int i=0; i < resultVector.size(); i++) {
-      str = co -> getOutputFromConsole("iwconfig | grep " + resultVector[i]);
-      if (str.find(myName) != string::npos) {
-        str = co -> getOutputFromConsole("sudo arp-scan --interface=" + resultVector[i] + " --localnet | awk 'FNR > 2 {print $1 \" \" $2}' | head -n -3");
-        cout << str << endl;
-      } else {
-        cout << "Not connected to WAN" << endl;
       };
     };
     return 0;
