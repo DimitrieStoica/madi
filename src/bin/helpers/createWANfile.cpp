ad2818468b30fdb8c988adfae8d551eb529a559d
Alex Stoica <stoica.dimitrie@gmail.com>
2016-02-25 17:54:59 +0000

    edit file output to look better

--- src/bin/helpers/createWANfile.cpp
+++ src/bin/helpers/createWANfile.cpp
@@ -13,13 +13,15 @@ class createWANfile {
     myfile << "id=" + myName + "\n";
     myfile << "uuid=" + co -> getOutputFromConsole("uuidgen");
     myfile << "type=802-11-wireless\n";
-    myfile << "autoconnect=false\n";
+    myfile << "\n";
     myfile << "[802-11-wireless]\n";
     myfile << "ssid=" + myName + "\n";
     myfile << "mode=ap\n";
-    myfile << "mac-address= " + co -> getOutputFromConsole("ifconfig " + networkHardware + " | awk '/HWaddr/ {print $5}'");
+    myfile << "mac-address=" + co -> getOutputFromConsole("ifconfig " + networkHardware + " | awk '/HWaddr/ {print $5}'");
+    myfile << "\n";
     myfile << "[ipv6]\n";
     myfile << "method=auto\n";
+    myfile << "\n";
     myfile << "[ipv4]\n";
     myfile << "method=shared";
     myfile.close();
